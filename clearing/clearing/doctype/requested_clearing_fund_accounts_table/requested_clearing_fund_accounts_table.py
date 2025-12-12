501eb8f73275cfffe0fab1efac0bfd1da6141fdc
symonmuchemi <simonkenya846@gmail.com>
2025-12-12 15:18:58 +0200

    feat: remove requested clearing fund accounts and payment details
    This commit deletes the 'Requested Clearing Fund Accounts Table' and 'Requested Clearing Payment Details' DocTypes, along with their associated files and configurations.

--- clearing/clearing/doctype/requested_clearing_fund_accounts_table/requested_clearing_fund_accounts_table.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Nelson Mpanju and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class RequestedClearingFundAccountsTable(Document):
-	pass
--- clearing/clearing/doctype/requested_clearing_payments/test_requested_clearing_payments.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Nelson Mpanju and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestRequestedClearingPayments(FrappeTestCase):
-	pass
