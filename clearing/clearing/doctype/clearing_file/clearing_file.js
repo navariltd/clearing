1b9dce38be0cbaeaf5ff0f41a054f1f1322bdb7c
symonmuchemi <simonkenya846@gmail.com>
2025-12-13 10:31:56 +0200

    update: removed implemented TODO comment

--- clearing/clearing/doctype/clearing_file/clearing_file.js
+++ clearing/clearing/doctype/clearing_file/clearing_file.js
@@ -749,7 +749,6 @@ frappe.ui.form.on("Clearing File Document", {
   },
 });
 
-// TODO: Make this configurable form settings
 function get_required_clearing_documents_js(mode_of_transport, callback) {
   if (!mode_of_transport) {
     if (callback) callback([]);
